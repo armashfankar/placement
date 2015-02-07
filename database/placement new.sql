e4e14945b96c501cf3cc465f520341cadd4b61e1
Armash Fankar <talktoarmash@gmail.com>
2015-02-07 16:47:38 +0530

    database file

--- database/placement new.sql
+++ database/placement new.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 20, 2015 at 09:47 PM
+-- Generation Time: Feb 07, 2015 at 04:45 PM
 -- Server version: 5.5.38-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.3
 
@@ -49,8 +49,7 @@ CREATE TABLE IF NOT EXISTS `comp_info` (
 --
 
 INSERT INTO `comp_info` (`cid`, `username`, `companyname`, `website`, `landline`, `fax`, `career`, `estb_year`, `address`, `hrname`, `email`, `mobile`) VALUES
-(2, 'fitrangi', 'Fitrangi ', 'www.fitrangi.com', '28198981', '2130889', 'Web Development', '2014', 'goregaon', 'armash', 'talktoarmash@gmail.com', '9967031001'),
-(4, 'mind', 'mind', 'mind.@aa.com', '222', '222', 'IT', '999', 'B/02,RAHAT APT.,BEHIND SHAHEEN HOSPITAL,KAUSA', 'armash', 'armash.fankar@yahoo.com', '9967031001');
+(32, 'mind', 'Mindcraft', 'www.midcraft.com', '12345', '1234', 'IT', '2010', 'mumbai', 'Vivek', 'vivek@xyz.com', '123456');
 
 -- --------------------------------------------------------
 
@@ -67,15 +66,44 @@ CREATE TABLE IF NOT EXISTS `comp_login` (
   `website` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`cid`),
   UNIQUE KEY `companyname` (`companyname`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 --
 -- Dumping data for table `comp_login`
 --
 
 INSERT INTO `comp_login` (`cid`, `username`, `password`, `companyname`, `approval`, `website`) VALUES
-(2, 'fitrangi', 'fitrangi', 'fitrangi', 'yes', 'www.fitrangi.com'),
-(4, 'mind', '*D982DF5FF1FC3DAA682BCB5A66BF3FC205D50115', 'mindcraft', 'yes', 'www.info.com');
+(32, 'mind', '*1A256E4E2FE95B8BF7349C168991EA8035D1359B', 'Mindcraft', 'yes', 'www.midcraft.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job`
+--
+
+CREATE TABLE IF NOT EXISTS `job` (
+  `jid` int(255) NOT NULL AUTO_INCREMENT,
+  `compname` varchar(255) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `domain` varchar(255) NOT NULL,
+  `job_location` varchar(255) NOT NULL,
+  `job_role` varchar(255) NOT NULL,
+  `salary` varchar(255) NOT NULL,
+  `ssc` varchar(255) NOT NULL,
+  `hsc` varchar(255) NOT NULL,
+  `diploma_agg` varchar(255) NOT NULL,
+  `deg_agg` varchar(255) NOT NULL,
+  `venue` varchar(255) NOT NULL,
+  `approval` varchar(255) NOT NULL,
+  PRIMARY KEY (`jid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `job`
+--
+
+INSERT INTO `job` (`jid`, `compname`, `department`, `domain`, `job_location`, `job_role`, `salary`, `ssc`, `hsc`, `diploma_agg`, `deg_agg`, `venue`, `approval`) VALUES
+(1, 'wipro', 'co', 'PHP', 'Bangalore', 'Develeoper', '30000', '50', '50', '50', '50', 'KTC', 'yes');
 
 -- --------------------------------------------------------
 
@@ -92,6 +120,14 @@ CREATE TABLE IF NOT EXISTS `news` (
   `contact` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`newsid`, `title`, `date`, `description`, `venue`, `contact`, `email`) VALUES
+(0, 'testing', '2015-02-05', '', '', '', 'hihaisdh'),
+(0, 'asdhash', '2015-02-20', 'asdadasd', 'KTC', '7890', 'talktoarmash@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -127,9 +163,8 @@ CREATE TABLE IF NOT EXISTS `student_info` (
 --
 
 INSERT INTO `student_info` (`sid`, `username`, `fullname`, `dob`, `gender`, `address`, `about_you`, `institute`, `university`, `department`, `batch`, `deg_sem7`, `deg_sem8`, `deg_agg`, `diploma_agg`, `hsc`, `ssc`, `key_skills`, `project_title`, `approval`) VALUES
-(5, 'nabeel', 'hawa nabeel', '01/01/1993', 'male', 'B/02,RAHAT APT.BEHIND SHAHEEN HOSPITAL,KAUSA,MUMBRA', 'TRAVELLING', 'KALSEKAR TECHNICAL CAMPUS', 'MUMBAI UNIVERSITY', 'COMPUTER ENGINEERING', 'JOB SERACH ENGINE', 68, 77, 67, 68, 0, 77, 'PHP,MYSQL,HTML', 'JOB SERACH ENGINE', ''),
-(8, 'armash', 'Armash Aslam Fankar', '14/09/1992', 'male', 'B/02,RAHAT APT.BEHIND SHAHEEN HOSPITAL,KAUSA,MUMBRA', 'TRAVELLING', 'KALSEKAR TECHNICAL CAMPUS', 'MUMBAI UNIVERSITY', 'COMPUTER ENGINEERING', '2014-2015', 60, 59, 63, 67, 0, 77, 'PHP,MYSQL,HTML', 'JOB SERACH ENGINE', ''),
-(9, 'saima', 'saima', '14/09/1992', 'female', 'wadala', 'TRAVELLING', 'KALSEKAR TECHNICAL CAMPUS', 'MUMBAI UNIVERSITY', 'COMPUTER ENGINEERING', '2014-2015', 78, 87, 78, 77, 87, 78, 'PHP,MYSQL,HTML', 'JOB SERACH ENGINE', '');
+(45, 'saima', 'Haji Saima', '12/1/1996', 'female', 'wadala', 'Student', 'Kalsekar techincal Campus', 'Mumbai university', 'computer', '2014-2015', 74, 80, 72, 84, 0, 86, 'HTML,PHP', 'Placement Portal', ''),
+(46, 'huma', '', '', '', '', '', '', '', 'computer', '', 0, 0, 0, 0, 0, 0, '.net', '', '');
 
 -- --------------------------------------------------------
 
@@ -145,14 +180,15 @@ CREATE TABLE IF NOT EXISTS `stud_login` (
   `approval` varchar(255) NOT NULL,
   PRIMARY KEY (`sid`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
 
 --
 -- Dumping data for table `stud_login`
 --
 
 INSERT INTO `stud_login` (`sid`, `username`, `email`, `password`, `approval`) VALUES
-(15, 'armash', 'talktoarmash@gmail.com', '*E452F6CB9075767B74485A3D137295EE647F823C', '');
+(45, 'saima', 'hajisaima9@gmail.com', '*1A256E4E2FE95B8BF7349C168991EA8035D1359B', 'yes'),
+(46, 'huma', 'huma@gmail.com', '*0F03B89645CDFC99CC2E4FC5CBA3CDD5E15AA746', '');
 
 -- --------------------------------------------------------
 
@@ -172,6 +208,28 @@ CREATE TABLE IF NOT EXISTS `tpo` (
 
 INSERT INTO `tpo` (`tid`, `username`, `password`) VALUES
 (1, 'tpo', 'tpo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tpo_info`
+--
+
+CREATE TABLE IF NOT EXISTS `tpo_info` (
+  `name` varchar(255) DEFAULT NULL,
+  `degree` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tpo_info`
+--
+
+INSERT INTO `tpo_info` (`name`, `degree`, `email`, `mobile`, `tid`) VALUES
+('P.S.Lokhande', 'PH.D ', 'pslokhande@gmail.com', '1234566', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
