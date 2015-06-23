ea235f646f10f93c7230c6b75d8ec5e4f0250aa2
armashfankar <talktoarmash@gmail.com>
2015-06-23 20:47:11 +0530

    new

--- test1.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$studentid = $_SESSION['s_id'];
-echo $studentid;
-?>
--- tpo/msearch.php
+++
@@ -1,7 +0,0 @@
-<?php
-$fullname=$_POST['fullname'];
-$department=$_POST['department'];
-
-echo $fullname;
-echo $department;
-?>
